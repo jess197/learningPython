67d0466437bb8530ec31ddc7ea71177251c40d62
jess197 <jessicacostaesilva97@gmail.com>
2021-10-09 21:39:01 -0300

    Jokenpo game

---
+++ exercise46.py
@@ -0,0 +1,57 @@
+## Develop a program to play jokenpo with you
+
+from random import choice
+from time import sleep
+print('=-'*20)
+print('JOKENPO GAME')
+print('Choose one option: ')
+print('1 - Rock')
+print('2 - Scissor')
+print('3 - Paper')
+print('=-'*20)
+playerChoice = int(input())
+
+if(playerChoice == 1):
+    playerChoice = 'Rock'
+elif(playerChoice == 2):
+    playerChoice = 'Scissor'
+elif(playerChoice == 3):
+    playerChoice = 'Paper'
+else:
+    print('Invalid option')
+
+computerOption = ['Rock','Scissor','Paper']
+
+computerChoice = choice(computerOption)
+print('JO KEN PO..')
+sleep(4)
+if(computerChoice == 'Paper'):
+  if(playerChoice == 'Rock'):
+      print('I WON, YOU LOST! I choose {} and you choose {}'.format(computerChoice, playerChoice))
+  elif(playerChoice == 'Scissor'):
+      print('CONGRATULATIONS YOU WON, I LOST! I choose {} and you choose {}'.format(computerChoice, playerChoice))
+  elif(playerChoice == 'Paper'):
+      print("It's a DRAW. We need to play again. I choose {} and you choose {} as well".format(computerChoice, playerChoice))
+elif(computerChoice == 'Rock'):
+    if (playerChoice == 'Rock'):
+        print("It's a DRAW. We need to play again. I choose {} and you choose {} as well".format(computerChoice, playerChoice))
+    elif (playerChoice == 'Scissor'):
+        print('I WON, YOU LOST! I choose {} and you choose {}'.format(computerChoice, playerChoice))
+    elif (playerChoice == 'Paper'):
+        print('CONGRATULATIONS YOU WON, I LOST! I choose {} and you choose {}'.format(computerChoice, playerChoice))
+elif(computerChoice == 'Scissor'):
+    if (playerChoice == 'Rock'):
+        print('CONGRATULATIONS YOU WON, I LOST! I choose {} and you choose {}'.format(computerChoice, playerChoice))
+    elif (playerChoice == 'Scissor'):
+        print("It's a DRAW. We need to play again. I choose {} and you choose {} as well".format(computerChoice, playerChoice))
+    elif (playerChoice == 'Paper'):
+        print('I WON, YOU LOST! I choose {} and you choose {}'.format(computerChoice, playerChoice))
+
+
+
+
+
+
+
+
+
