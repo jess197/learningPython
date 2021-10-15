56157d203297a8f2446cfe581f1b129454928839
jess197 <jessicacostaesilva97@gmail.com>
2021-10-14 21:05:05 -0300

    Adjustment

--- exercise48.py
+++ exercise48.py
@@ -2,9 +2,8 @@
 ## Develop a program that shows just even numbers on screen in range of 0 to 50
 print('=-'*50)
 print('EVEN NUMBERS:')
-for n in range(0,50+1,2):
-    if(n < 50):
-        print('{},'.format(n),end='')
+for n in range(0,51,2):
+    print('{},'.format(n),end='')
 print(n)
 print('=-'*50)
 
